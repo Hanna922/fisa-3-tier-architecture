9cc437729629d83f2597d99a22dc1666b213bc47
urimL <toulim0525@gmail.com>
2026-02-27 21:34:40 +0900

    feat: login filter 기능 추가 및 버튼 연동

---
+++ sample-project/src/main/java/dev/sample/filter/AuthFilter.java
@@ -0,0 +1,35 @@
+package dev.sample.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter({"/life-stages", "/info", "/admin/*"})
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+        boolean loggedIn = session != null && session.getAttribute("loginUser") != null;
+
+        if (!loggedIn) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
