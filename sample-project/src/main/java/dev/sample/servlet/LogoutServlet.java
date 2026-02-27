6976b90ae382168ad38d6665770bea78778add46
Hanna922 <p.ping0922@gmail.com>
2026-02-27 23:08:11 +0900

    fix: remove browser cookie when logout

--- sample-project/src/main/java/dev/sample/servlet/LogoutServlet.java
+++ sample-project/src/main/java/dev/sample/servlet/LogoutServlet.java
@@ -10,7 +10,14 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         HttpSession session = req.getSession(false);
+
         if (session != null) session.invalidate();
+
+        Cookie cookie = new Cookie("JSESSIONID", "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        resp.addCookie(cookie);
+
         resp.sendRedirect(req.getContextPath() + "/login");
     }
 }
